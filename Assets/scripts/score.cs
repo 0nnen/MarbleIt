fffbdb11769a7d3225a225d5a4bf5a2d19804738
cbobeda <cbobeda@gaming.tech>
2024-11-21 20:58:04 +0100

    Coinsv1

---
+++ Assets/scripts/score.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+    private int coins = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int getCoins()
+    {
+        return coins;
+    }
+    public void setCoins()
+    {
+        coins++;
+    }
+}
